bef2ce7d1d40475bc3d18612a3bd61cb16dba758
Russell Ahlstrom <rahlstrom@deseretdigital.com>
2014-12-08 14:18:44 -0700

    Corrects Gulp ghpages Tasks with Right Directory Glob
    The src glob for the gulp ghpages task was wrong. Updates it from
    /example to /example/**/*.

--- gulp/tasks/ghpages.js
+++ gulp/tasks/ghpages.js
@@ -2,6 +2,6 @@ var gulp = require('gulp');
 var ghpages = require('gulp-gh-pages');
 
 gulp.task('ghpages', ['build'], function () {
-  return gulp.src('./example')
+  return gulp.src('./example/**/*')
     .pipe(ghpages());
 });
